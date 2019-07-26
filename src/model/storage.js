cb0586e950bf205a6504261edbabb3ce506c0914
JiangYuan2899 <445369505@qq.com>
2019-07-26 01:04:07 -0700

    first

---
+++ src/model/storage.js
@@ -0,0 +1,17 @@
+//封装操作localstorage本地存储的方法 模块化文件
+
+//node.js基础  内容
+
+var storage ={
+	set(key,value){
+		localStorage.setItem(key,JSON.stringify(value));
+	},
+	get(key){
+
+		return JSON.parse(localStorage.getItem(key));
+	},remove(key){
+		localStorage.removeItem(key);
+	} 
+}
+
+export default storage;
